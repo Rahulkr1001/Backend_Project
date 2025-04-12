55f4ebec089e24bb51440f0cf268b7fb877c097a
Rahul Kumar <rahulkrsharma3580@gmail.com>
2025-04-13 04:27:51 +0530

    Utility

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,29 @@
+//Method 1
+const asyncHandler = (requesthandler) => {
+    return (req, res, next) => {
+        promise.resolve(requesthandler(req, res, next)).
+        catch((error) => next(error));
+    }
+}
+
+
+export { asyncHandler }
+
+
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async() => {}
+
+// Methed 2
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try{
+//         await fn(req, res, next);
+//     } catch(error){
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         });
+//     }
+// }
